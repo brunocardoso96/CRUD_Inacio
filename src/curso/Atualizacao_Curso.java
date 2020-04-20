c3a8fd98a7b70b8105a5bea053b0291109599b1f
Bruno Cardoso <brunowcnascimento@gmail.com>
2020-04-20 18:06:39 -0300

    Atualização_Curso alterei uma variavel de String para Long e fazendo a conversão na mesma linha e deletei comentarios

--- src/curso/Atualizacao_Curso.java
+++ src/curso/Atualizacao_Curso.java
@@ -6,17 +6,12 @@ import javax.swing.JOptionPane;
 public class Atualizacao_Curso {
 
 	public static void main(String[] args) {
-		/*
-		String paramId = JOptionPane.showInputDialog("Entre com o id a ser atualizado: ");
-		String paramFirstName = JOptionPane.showInputDialog("Entre com o novo primeiro nome: ");
-		*/
+		
 		Banco_Curso.Select();
-		String paramId = JOptionPane.showInputDialog("Entre com o id a ser atualizado: ");
+		Long paramId = Long.parseLong(JOptionPane.showInputDialog("Entre com o id a ser atualizado: "));
 		String paramNome = JOptionPane.showInputDialog("Entre com o novo nome: ");
-		
-		Long id = Long.parseLong(paramId);
-		
-		Banco_Curso.Update(id, paramNome);
+
+		Banco_Curso.Update(paramId, paramNome);
 		Banco_Curso.Select();
 		
 	}
