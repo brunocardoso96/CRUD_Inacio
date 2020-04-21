4e62754ecbc31eb67f15ea68a5156e3acfb7b22c
Bruno Cardoso <brunowcnascimento@gmail.com>
2020-04-21 02:07:14 -0300

    Deixando um Main para executar função do Professor e enviando para pacote Model

---
+++ src/Model/ModelProfessor.java
@@ -0,0 +1,75 @@
+package Model;
+
+import javax.swing.JOptionPane;
+
+import Controller.Banco_Aluno;
+import Controller.Banco_Professor;
+
+public class ModelProfessor {
+
+	public static void main(String[] args) {
+		
+		
+		
+		Integer valor = 0;
+		
+		do {
+			
+			valor = Integer.parseInt((JOptionPane.showInputDialog
+												(" ---- PROFESSOR ----\n "
+												+"1 - SELECT \n "
+												+"2 - INSERT\n "
+												+"3 - UPDATE\n "
+												+"4 - DELETE\n "
+												+"5 - SAIR")));
+			switch (valor) {
+			
+				case 1:
+					Banco_Professor.Select();
+					break; 
+					
+				case 2:
+					
+					String paramNome = JOptionPane.showInputDialog("Entre com o nome do Professor: ");
+					Banco_Professor.Insert(paramNome);
+					Banco_Professor.Select();
+					break;
+					
+				case 3:
+					
+					Banco_Professor.Select();
+					String paramId = JOptionPane.showInputDialog("Entre com o id a ser atualizado: ");
+					String paramNomeUpdate = JOptionPane.showInputDialog("Entre com o novo nome: ");
+					
+					Long id = Long.parseLong(paramId);
+					
+					Banco_Professor.Update(id, paramNomeUpdate);
+					Banco_Professor.Select();
+					
+					break;
+					
+				case 4:
+					
+					Banco_Professor.Select();
+					Long paramIdDelete = Long.parseLong(JOptionPane.showInputDialog("Entre com o id: "));
+					Banco_Professor.Delete(paramIdDelete);
+					Banco_Professor.Select();
+					break;
+
+				default: 
+					if(valor < 1 || valor > 5) { 
+						JOptionPane.showMessageDialog(null, "Foi digitador um CODIGO Invalido");
+					}
+					break;
+			}
+			
+			
+			
+		} while( valor != 5 );
+		
+			JOptionPane.showMessageDialog(null, "Finalizado CRUD - PROFESSOR");	
+	
+
+	}
+
+}
